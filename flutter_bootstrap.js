130960363a7e744e9a75ec4437d5939b52f989d9
unknown <parames@buson.in>
2024-10-14 18:31:57 +0530

    LAst commit 9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3876289849"
+    serviceWorkerVersion: "1041492474"
   }
 });
